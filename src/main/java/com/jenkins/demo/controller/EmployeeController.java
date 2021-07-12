288142edf58c597ccdd95e9672a8605385b079e1
meenalgarg <meenalgarg2610@gmail.com>
2021-07-12 22:28:25 +0530

    adding project

---
+++ src/main/java/com/jenkins/demo/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.jenkins.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jenkins.demo.entity.Employee;
+
+@RestController
+public class EmployeeController {
+
+	
+	@GetMapping("/")
+	public String homePage() {
+		return "Home page";
+	}
+	
+	@GetMapping("/employees")
+	public List<Employee> getEmployees(){
+		List<Employee> employees = new ArrayList<Employee>();
+		
+		Employee emp = new Employee();
+		emp.setId(1);
+		emp.setName("meenal");
+		emp.setDepartment("IT");
+		
+		employees.add(emp);
+		
+		return employees;
+	}
+	
+}
